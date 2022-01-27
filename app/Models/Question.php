7b2f1c5ba60e249e95e1e4f7d79c334c4f72b87f
mostafa kamal <mostafa.kamal823@yahoo.com>
2022-01-27 15:23:05 +0200

    relationShips

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Reply;
+use App\Models\Category;
 
 class Question extends Model
 {
     use HasFactory;
+
+    public function user(){
+      return $this->belongsTo(User::class , 'user_id' , 'id');
+    }
+
+    public function replies(){
+      return $this->hasMany(Reply::class , 'replay_id' , 'id');
+    }
+
+    public function category(){
+         return $this->belongsTo(Category::class , 'category_id' , 'id');
+    } 
 }
--- app/Models/Reply.php
+++ app/Models/Reply.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Question;
+use App\Models\User;
 
 class Reply extends Model
 {
     use HasFactory;
+
+    public function question(){
+        return $this->belongsTo(Question::class , 'question_id' , 'id');
+    } 
+
+    public function user(){
+        return $this->belongsTo(User::class , 'user_id' , 'id');
+    }
+
+   public function likes(){
+       return $this->hasMany('likes' , 'like_id' , 'id');
+   }
 }
